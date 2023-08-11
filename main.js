2caf091c7577d573943e563425aa49cb69485b44
Gui1304 <130668551+Gui1304@users.noreply.github.com>
2023-08-11 16:08:34 -0300

    commit do Guilhermão

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let saudacao = "Bom dia!"
+let saudacao = "";
+const horaAtual = new Date().getHours();
+const imagem = document.getElementById("imagem");
 
-console.log(saudacao)
+if (horaAtual >= 6 && horaAtual < 12) {
+    saudacao = "Bom dia!";
+    imagem.src = "imagem-dia.jpg"; 
+} else if (horaAtual >= 12 && horaAtual < 18) {
+    saudacao = "Boa tarde!";
+    imagem.src = "imagem-tarde.jpg"; 
+} else {
+    saudacao = "Boa noite!";
+    imagem.src = "imagem-noite.jpg"; 
+}
 
-document.getElementById("resultado").innerHTML = saudacao
+document.getElementById("resultado").textContent = saudacao;
